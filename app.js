86959b4d2852c9e30c1e473576fcf80909849111
Ryan Lundy <rlundy1792@gmail.com>
2023-05-09 12:23:10 -0400

    Completed basic CRUD functionality for meals SQL TABLE

---
+++ app.js
@@ -0,0 +1,24 @@
+// dependencies
+const express = require("express");
+const cors = require("cors");
+const mealsControllers = require("./controllers/mealsControllers");
+
+// config
+const app = express();
+
+// middleware
+app.use(cors())
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+    res.send("Welcome to the Snack-a-Log App")
+});
+
+app.use("/meals", mealsControllers);
+
+app.get("*", (req, res) => {
+    res.status(404).send("Page Not Found");
+});
+
+module.exports = app;
