ea2b99fb68e2b84a6648912f0d29bdeabe778b08
Ryan Lundy <rlundy1792@gmail.com>
2023-05-15 01:40:18 -0400

    Changed query for 'getUserMeals' in 'userMeals' query file to 'SELECT * FROM meals WHERE user_id = ;'

--- queries/userMeals.js
+++ queries/userMeals.js
@@ -3,11 +3,7 @@ const db = require("../db/dbConfig")
 const getUserMeals = async (userId) => {
     try {
         const userMealsData = await db.any(
-            `SELECT meals.*, users.*
-              FROM meals
-              JOIN users ON meals.user_id = users.id
-              WHERE meals.user_id = $1
-              AND users.id = $1;`,
+            `SELECT * FROM meals WHERE user_id = $1;`,
             userId);
         return { userMealsData }
     } catch (error) {
